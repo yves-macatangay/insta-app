12eca264efdf9f31936936418c42e055646abd7d
Full Name <dsad>
2025-01-28 21:02:09 +0800

    force https

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -5,6 +5,7 @@ namespace App\Providers;
 use Illuminate\Support\ServiceProvider;
 use Illuminate\Pagination\Paginator;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Support\Facades\URL;
 use App\Models\User;
 
 class AppServiceProvider extends ServiceProvider
@@ -28,6 +29,8 @@ class AppServiceProvider extends ServiceProvider
             return $user->role_id == User::ADMIN_ROLE_ID;
         });
 
-        
+        if($this->app->environment('production')){
+            URL::forceScheme('https');
+        }
     }
 }
